4b212ae4e433940812e94580ce11499b8a9eaa24
HenriqueDaMotaCorrea <riquelhama@gmail.com>
2023-07-13 18:00:18 -0300

    Added command handling

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 # Wumpy
 # Based on Hunt The Wumpus by Gregory Yob
 
-def input_handler(raw_input):
-    proc_input = str.upper(raw_input)
-    return proc_input
+def input_handler(raw_in):
+    proc_in = str.upper(raw_in)
+    return proc_in
+
+def command_handler(cmd_dict, cmd_name):
+    return cmd_dict.get(cmd_name, show_help)
+
+def show_help():
+    print("""
+          HELP: displays available commands and instructions.
+          QUIT: exits the game.
+        """)
+
+commands = {
+    'QUIT': SystemExit,
+    'HELP': show_help,
+}
 
 def game():
     while True:
@@ -16,5 +30,5 @@ def main():
     game()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
--- test_batch.py
+++ test_batch.py
@@ -1,4 +1,5 @@
-# Unit tests
+# Tests for TDD
+# To run tests: enter "pytest" in terminal
 
 import pytest
 from main import *
@@ -6,3 +7,24 @@ from main import *
 def test_input_handler():
     test_inp = input_handler('test')
     assert test_inp == 'TEST'
+
+#TODO: Add parameter for defining default match
+def test_command_handler_function_matching():
+    def test_command():
+        return 'test'
+
+    test_dict = {
+        'TEST': test_command
+    }
+    test_cmd = command_handler(test_dict, 'TEST')
+    assert test_cmd == test_command
+
+def test_command_handler_default_matching():
+    def test_command():
+        return 'test'
+
+    test_dict = {
+        'TEST': test_command
+    }
+    test_cmd = command_handler(test_dict, 'NOTTEST')
+    assert test_cmd == show_help
