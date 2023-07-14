cbb81cbd3a1741ccd2385c2d748fa6a981ec808f
HenriqueDaMotaCorrea <riquelhama@gmail.com>
2023-07-14 03:08:23 -0300

    Added commands, polished command handling, polished tests

--- main.py
+++ main.py
@@ -1,34 +1,41 @@
 # Wumpy
 # Based on Hunt The Wumpus by Gregory Yob
 
+help_text = """
+    HELP: displays available commands and instructions.
+    QUIT: exits the game.
+"""
+
+intro_text = """
+    Welcome to Wumpy!
+    Type HELP for instructions.
+"""
+
+def show_help():
+    print(help_text)
+
+def quit_game():
+    raise SystemExit()
+
 def input_handler(raw_in):
     proc_in = str.upper(raw_in)
     return proc_in
 
-def command_handler(cmd_dict, cmd_name):
-    return cmd_dict.get(cmd_name, show_help)
-
-def show_help():
-    print("""
-          HELP: displays available commands and instructions.
-          QUIT: exits the game.
-        """)
-
-commands = {
-    'QUIT': SystemExit,
-    'HELP': show_help,
-}
+def command_handler(cmd_dict, cmd_name, default_cmd=None):
+    return cmd_dict.get(cmd_name, default_cmd)
 
 def game():
+    commands = {
+        'HELP': show_help,
+        'QUIT': quit_game
+    }
     while True:
-        print('Type QUIT to leave:')
-        command = input_handler(input('>'))
-        if command == 'QUIT':
-            raise SystemExit()
+        print(intro_text)
+        cmd_in = input_handler(input('>'))
+        command_handler(commands, cmd_in, show_help)()
 
 def main():
     game()
-
 
 if __name__ == '__main__':
     main()
--- test_batch.py
+++ test_batch.py
@@ -8,10 +8,9 @@ def test_input_handler():
     test_inp = input_handler('test')
     assert test_inp == 'TEST'
 
-#TODO: Add parameter for defining default match
 def test_command_handler_function_matching():
     def test_command():
-        return 'test'
+        pass
 
     test_dict = {
         'TEST': test_command
@@ -21,10 +20,13 @@ def test_command_handler_function_matching():
 
 def test_command_handler_default_matching():
     def test_command():
-        return 'test'
+        pass
+    
+    def test_default_func():
+        pass
 
     test_dict = {
         'TEST': test_command
     }
-    test_cmd = command_handler(test_dict, 'NOTTEST')
-    assert test_cmd == show_help
+    test_cmd = command_handler(test_dict, 'NOTTEST', test_default_func)
+    assert test_cmd == test_default_func
