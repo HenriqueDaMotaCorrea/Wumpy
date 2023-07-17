cd76f54c5bb38eb197f70d889d4a11c4fd7e0ae9
HenriqueDaMotaCorrea <riquelhama@gmail.com>
2023-07-17 04:22:26 -0300

    Added help text with commands and instructions

--- texts.py
+++ texts.py
@@ -2,14 +2,52 @@
 
 #TODO: Add translation support
 
-text_invalid = """What? Please try again."""
+text_invalid = """What? (Enter [H]elp for instructions.)"""
 
 text_help = """
-Instructions coming soon!"""
+Wumpy - Hunt the Wumpus in Python
+
+Commands:
+    - [Q]uit: exit the program
+    - [H]elp: display commands and instructions
+    - [M]ove: prompt to select a room to move into
+    - [S]hoot: prompt to select a room to shoot into
+
+Instructions:
+
+    [YOU, THE PLAYER]
+    You are hunting a Wumpus in its cave. The cave is made up of 20 rooms, 
+    connected as the vertices of a dodecahedron. That means each room connects 
+    to 3 other rooms.
+    Every turn, you can choose to either MOVE or SHOOT an arrow into a room 
+    that's connected to the one you're currently in.
+    
+    [PITS]
+    Some rooms contain bottomless pits. You will FEEL A DRAFT if near one.
+    If you enter a room with a pit, you'll plummet to your doom! Stay alert!
+
+    [BATS]
+    Bat swarms make the cave their home, you'll HEAR FLAPPING if they're close.
+    If you disturb a swarm, they'll snatch you up and drop you somewhere else!
+
+    [WUMPUS]
+    Your objective is to find the Wumpus, and shoot an arrow into the room it 
+    sleeps in. You will SMELL it when you get close.
+    The Wumpus sleeps most of the time, but will wake up if:
+    - You enter its room
+    - You shoot an arrow and miss
+    Upon waking up, the Wumpus has a 3 in 4 chance of moving to an adjacent 
+    room, and a 1 in 4 chance of staying where it is.
+    The Wumpus won't get carried by bats, nor fall down pits.
+    If you enter the Wumpus' room and it stays, or if it moves into the room 
+    you're in, you're toast!
+
+    You know what to do now, so get out there and HUNT THE WUMPUS!
+"""
 
 text_intro = """
 Welcome to Wumpy!
-Enter HELP/H for instructions, or QUIT/Q to leave."""
+Enter [H]elp for instructions, or [Q]uit to leave."""
 
 # Warnings
 text_wumpus_msg = """That smell... the Wumpus is close by!"""
@@ -42,8 +80,7 @@ Didn't hit anything..."""
 
 # Standard
 text_roomdesc = """
-You are in room {}. Exits to: {}
-(MOVE/M to move, SHOOT/S to shoot)"""
+You are in room {}. Exits to: {} ([M]ove/[S]hoot)"""
 
 text_wheremove = """Room to go to? """
 
